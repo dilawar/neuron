1144fe802b2fec727b1bdaba5ed24349e8963aed
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-04-22 11:34:37 +0530

    Need more coffee.

--- synapse/test_exp_syn.cpp
+++ synapse/test_exp_syn.cpp
@@ -21,28 +21,58 @@
 #include "ExpSynapse.h"
 #include <random>
 #include <chrono>
+#include <random>
 
 SC_MODULE(TestExpSyn) 
 {
     sc_in<bool> clock;
 
-    // Calcium pulse into syanpse.
-    sc_signal<bool> in, out;
+    // Spike goes into synapse.
+    sc_signal<bool> in;
+
+    // A voltage comes out of synapse.
+    sc_signal<double> out;
 
     void do_test() 
     {
-        cout << "Test " << endl;
+        while(true)
+        {
+            wait(dist_(gen_), SC_MS);
+            in = true;
+            wait(1, SC_MS);
+            in = false;
+        }
     }
 
-    SC_CTOR(TestExpSyn) {
-        SC_METHOD(do_test);
+    void process()
+    {
+        std::cout << "ticking..." << in << std::endl;
+
+    }
+
+    SC_CTOR(TestExpSyn) 
+    {
+        SC_THREAD(do_test);
+
+        SC_METHOD(process);
         sensitive << clock.neg();
 
         // dut
         // ExpSynapse dut("ExpSynapse");
         // dut.pre_synaptic_event(in);
         // dut.post_synaptic_event(out);
+        gen_.seed(rd_());
+        dist_.param(std::poisson_distribution<int>::param_type {5});
     }
+
+    // Methods
+
+
+    // Data members.
+    std::random_device rd_;
+    std::mt19937 gen_;
+    std::poisson_distribution<> dist_;
+
 };
 
 int sc_main(int argc, char *argv[])
