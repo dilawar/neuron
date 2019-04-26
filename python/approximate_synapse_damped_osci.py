bdb35776eea4be78d823863f1e13697e9f5487fc
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-04-26 20:46:13 +0530

    Will fix later.

--- python/approximate_synapse_damped_osci.py
+++ python/approximate_synapse_damped_osci.py
@@ -15,12 +15,12 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 
-tau1 = 1e-3
-tau2 = 1e-3
+tau = 5e-4
 gb   = 1e-9
 
 spks = [0e-3, 3e-3, 5e-3, 10e-3, 50e-3]
 spikes_ = spks[:]
+dt_ = 1e-3
 
 # approximate spikes.
 spikeX, spikeY, spikeDt = [], [], 0.2e-3
@@ -30,18 +30,19 @@ for s in spikes_:
 spikeF = interp1d(spikeX, spikeY, kind='slinear', fill_value='extrapolate' )
 
 def spike(t, spikes):
-    return spikeF(t)
-    #if not spikes:
-    #    return 0.0
-    #if t >= spikes[0]:
-    #    print(f'@{t}')
-    #    spikes.pop(0)
-    #    return 1.0
-    #return 0.0
+    global dt_
+    #  return spikeF(t)
+    if not spikes:
+        return 0.0
+    if t >= spikes[0]:
+        delay = t - spikes[0]
+        print(f'@{t} after {delay/dt_:g}')
+        spikes.pop(0)
+        return 1.0
+    return 0.0
 
 def alpha(t):
     spikes1_ = spks.copy()
-    tau = tau1
     g = np.zeros_like(t)
     for ts in spikes1_:
         tt = t - ts
@@ -51,41 +52,35 @@ def alpha(t):
     return g
 
 def pure_alpha(t):
-    tau = tau1
     return gb * t / tau * np.exp(-t/tau)
-
 
 def damped_oscillator(t, y, zeta=1.0):
     """damped_oscillator
-
     System driven by external force Fext.
-
-        d^2x(t)         dx(t)
-    ω^2 -------  + 2ζω  -----  + ωx = Fext(t)
-         dt^2            dt 
-
-    ω and ζ  can be computed using τ1 and τ2. 
-
-    ω = 1/√τ₁τ₂ and ζ=0.5(τ₁+τ₂)ω
-
-    :param t:
-    :param y:
-    :param zeta:
     """
-    w = (tau1*tau2)**-0.5
-    z = 0.5*(tau1+tau2)*w
-    assert w > 0
-    assert z >= 1.0
-    tau = tau1
-    return [y[1], -2*z*w*y[1] - w**2*y[0] + gb*2*w*w*spike(t, spikes_)]
+    return [y[1], (-2*tau*y[1] - y[0] + gb*spike(t, spikes_))/tau/tau ]
 
 tmax = 0.07
 plt.figure(figsize=(12,6))
-for dt in [1e-3, 3e-4, 5e-4, 1e-4, 5e-5]:
+ax1 = plt.subplot(211)
+ax2 = plt.subplot(212)
+
+tvec = np.linspace(0, tmax, 1000)
+refY = alpha(tvec)
+ax1.plot(tvec, refY, '--', lw=1, color='black', label='alpha')
+ax21 = ax2.twinx()
+ax21.plot(tvec, refY, '--', lw=1, color='black', label='alpha')
+ax1.legend()
+
+for dt in [1e-4, 1e-5, 1e-6]:
+    dt_ = dt
+    print( f"[INFO ] Using dt={dt:g}" )
     spikes_ = spks.copy()
     sol = solve_ivp(damped_oscillator, (0, tmax), (0.0,0.0), max_step=dt)
-    plt.plot(sol.t, sol.y[0,:], label = f'ode dt={dt:g}')
-plt.plot(sol.t, alpha(sol.t), '--', lw=1, color='black', label='alpha')
-plt.legend()
+    y = sol.y[0,:]
+    ax2.plot(sol.t, y, label = f'ode dt={dt:g}. r={refY.mean()/y.mean():g}')
 
-plt.show()
+ax2.legend()
+plt.tight_layout()
+plt.savefig( f'{__file__}.png' )
+
