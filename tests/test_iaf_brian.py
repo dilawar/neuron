39254157c109f8c24219381393b8eace640fc6a2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-05-02 17:19:38 +0530

    Fix it at home when wife makes ginger tea... mum mum ... She promise!!!! Thursday 02 May 2019 05:19:57 PM IST

--- tests/test_iaf_brian.py
+++ tests/test_iaf_brian.py
@@ -3,20 +3,63 @@ from brian2 import *
 
 
 def main():
-    P = PoissonGroup(1, 50*Hz)
-    group = NeuronGroup(1, 'dv/dt=-v/(10*ms): volt'
-            , threshold='v>-50*mV'
-            , reset='v=-65*mV'
-            )
-    S = Synapses(P, group, on_pre='v+=0.1*mV')
-    S.connect('i==j')
+    N = 1
+    P = PoissonGroup(N, 20*Hz)
+    Cm = 100*pF
+    Rm = 100e6*ohm
 
-    monitor = SpikeMonitor(group)
-    duration = 1*second
-    run(duration)
-    plot(group.v/mV, monitor.count / duration)
-    xlabel('v (mV)')
-    ylabel('Firing rate (sp/s)')
+    print( f"[INFO ] Tau is {Cm*Rm} sec" )
+    Erest = -65*mV
+
+    # excitatory synapses.
+    gEbar = 10 * nS
+    gIbar = 20 * nS
+    tauE = 10 * ms
+    tauI = 20* ms
+
+    VeSyn = 0.0*mV    # excitatory reversal potential.
+    ViSyn = -70*mV    # inhibitory reversal potential.
+    G = NeuronGroup(N
+            , '''
+                dv/dt=-(v-Erest)/(Rm*Cm) - (inject/Cm): volt
+                inject =  gE*(v-VeSyn) + gI*(v-ViSyn) : amp
+                gE : siemens
+                gI : siemens
+                '''
+            , threshold = 'v>-50*mV'
+            , reset = 'v=-65*mV'
+            )
+    G.v = -65*mV
+
+    SE = Synapses(P, G
+            , model = 'preT : second'
+            , on_pre='''
+                gE=gEbar*exp(-(t-preT)/tauE)
+                preT = t
+            '''
+            )
+    SE.connect('i==j')
+
+    # inhibitory synapse.
+    SI = Synapses(P, G
+            , model = 'preT : second'
+            , on_pre='''
+                gI=gIbar*exp(-(t-preT)/tauI)
+                preT = t
+            '''
+            )
+    SI.connect('i==j')
+
+    M = StateMonitor(G, 'v', record=True)
+    SM = SpikeMonitor(G)
+
+    run(10*second)
+    plt.subplot(211)
+    plot(M.t, M.v.T)
+    plt.subplot(212)
+    plot(SM.t, SM.i, '.')
+    xlabel('t')
+    ylabel('v')
     show()
 
 if __name__ == '__main__':
