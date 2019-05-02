de1460722cdfd4f2494fd50ff132c8151598abbb
Dilawar Singh <dilawars@ncbs.res.in>
2019-05-02 20:43:06 +0530

    same as c++ simulation.

--- tests/brian2/iaf.py
+++ tests/brian2/iaf.py
@@ -1,22 +1,22 @@
 from brian2 import *
 
-
-
 def main():
-    N = 2
+    N = 100
 
     # input
-    P = NeuronGroup(N, 'dx/dt= 20*Hz : 1', threshold='x>1', reset='x=0')
-    #P = PoissonGroup(N, 20*Hz)
+    #  P = NeuronGroup(N, 'dx/dt= 20*Hz : 1', threshold='x>1', reset='x=0')
+    P = NeuronGroup(N, 'rates :Hz', threshold='rand()<rates*dt')
+    P.rates = 20*Hz
+    #  P = PoissonGroup(N, 20*Hz)
 
-    Cm = 100*pF
+    Cm = 100e-12*farad
     Rm = 100e6*ohm
 
     print( f"[INFO ] Tau is {Cm*Rm} sec" )
     Erest = -65*mV
 
     # excitatory synapses.
-    gEbar = 20 * nS
+    gEbar = 30 * nS
     gIbar = 20 * nS
     tauE = 10 * ms
     tauI = 30 * ms
@@ -56,19 +56,19 @@ def main():
     SI.connect('i==j')
 
     SM = SpikeMonitor(G, variables=['v'])
-    SN = SpikeMonitor(P, variables=['x'])
+    SN = SpikeMonitor(P) #, variables=['x'])
     M = StateMonitor(G, 'v', record=True)
 
     run(2*second)
 
     plt.subplot(211)
-    plot(M.t, M.v.T)
+    #plot(M.t, M.v.T)
     plt.xlabel('t')
     plt.ylabel('v')
 
     plt.subplot(212)
     plot(SM.t, SM.i, '.')
-    plot(SN.t, SN.i, '.')
+    #plot(SN.t, SN.i, '.')
     plt.show()
 
 if __name__ == '__main__':
