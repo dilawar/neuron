9856448dbbfe2b50574e6b4acf6db7378c34b05a
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-05-06 15:25:54 +0530

    Link only static library.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// =====================================================================================
+//
+//       Filename:  tantrika.cpp
+//
+//    Description:  tantrika library.
+//
+//        Version:  1.0
+//        Created:  01/05/19 08:55:12 PM IST
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Dilawar Singh (), dilawar.s.rajput@gmail.com
+//   Organization:  NCBS Bangalore
+//
+// =====================================================================================
+
+#include "tantrika.h"
+
+int test_all(int argc, char* argv[])
+{ 
+    std::cout << "Warning: This should be empty." << std::endl;
+    return 0;
+}
+
